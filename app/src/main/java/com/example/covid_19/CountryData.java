f2023889b0f7baf50f29a4f6c4dda78198ea836b
RendiJuli <juliarto.rendi@gmail.com>
2021-02-02 23:13:08 +0700

    first UI

---
+++ app/src/main/java/com/example/covid_19/CountryData.java
@@ -0,0 +1,20 @@
+package com.example.covid_19;
+
+public class CountryData {
+
+        public static final String[] countryNames={
+
+                "INA","IND","USA","AFG","MY","MF"
+
+        };
+
+        public static final int[] countryFlags={
+
+                R.drawable.indonesia,
+                R.drawable.india,
+                R.drawable.uk,
+                R.drawable.afganistan,
+                R.drawable.malesiya,
+                R.drawable.maxico
+    };
+}
